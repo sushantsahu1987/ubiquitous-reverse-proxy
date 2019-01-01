a2f2400ac8cd0ead9bc63ec197bfc45fd6cff96c
Sushant Sahu <sushant87@gmail.com>
2019-01-01 19:33:36 +0530

    code update

--- simple-reverse-proxy/index.js
+++ simple-reverse-proxy/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.all('/', (req, resp, next)=> {
+app.all('/*', (req, resp, next) => {
 
+    console.log(` request hostname: ${req.hostname}`);
+    console.log(` request original url: ${req.originalUrl}`);
+    console.log(` request path: ${req.path}`);
+    console.log(` request subdomains: ${req.subdomains}`);
+
+    resp.send({
+        max: 'result'
+    });
 });
 
-app.listen(8080, ()=> {
+app.listen(8080, () => {
     console.log('server running on port 80');
 });
-
